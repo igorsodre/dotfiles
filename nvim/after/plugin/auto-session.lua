0df0d3883afae4fcb22a139d38fa2f9f691c61a9
Igor Sodré <csodreigor@gmail.com>
2023-08-16 20:25:58 -0300

    fix nerdtree bug with auto-session

--- nvim/after/plugin/auto-session.lua
+++ nvim/after/plugin/auto-session.lua
@@ -1,2 +1,8 @@
 require("auto-session").setup()
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+-- vim.g.auto_session_pre_save_cmds = vim.api.nvim_eval('["tabdo NERDTreeClose"]')
+vim.api.nvim_exec(
+[[
+let g:auto_session_pre_save_cmds = ["NERDTreeClose"]
+]],
+true)
